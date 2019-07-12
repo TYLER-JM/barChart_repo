0378ad04e62c89da806e4fd8f850f408fd7572aa
Tyler <t.mar92@gmail.com>
2019-07-12 23:50:47 +0000

    code to convert data points into percentages to use as heights for each bar

--- barChart.js
+++ barChart.js
@@ -2,21 +2,39 @@
 $(function() {
   let drawBarChart = function(data, options, element) {
 
-    //helper function to help create new elements
-    let makeEl = function(type, className) {
-      let string = "<" + type;
-      if (className === undefined) {
-        string += "></" + type + ">";
-      } else {
-        string += " class='" + className + "'></" + type + ">";
-      }
-      return string;
-    };
+  //helper function to help create new elements
+  let makeEl = function(type, className) {
+    let string = "<" + type;
+    if (className === undefined) {
+      string += "></" + type + ">";
+    } else {
+      string += " class='" + className + "'></" + type + ">";
+    }
+    return string;
+  };
 
-    var container = $(makeEl("div", "chart-container"));
-    var chart = $(makeEl("div", "chart"));
+  var container = $(makeEl("div", "chart-container"));
+  var chart = $(makeEl("div", "chart"));
 
-    container.appendTo(element);
-    chart.appendTo(container);
+  container.appendTo(element);
+  chart.appendTo(container);
+
+  //convert data points into percentages in order to use as heights
+  //store the percentages into an array scaledValues
+  let scaledValues = [];
+  for (let i = 0; i < data.length; i++) {
+    if (typeof data[i].value === "object") {
+      let subConverts = data[i].value.map(function(x) {
+        let converted  = ((x - options[0].start) / (options[0].end - options[0].start) * 100).toFixed(2) + "%";
+        return converted;
+      });
+      scaledValues.push(subConverts);
+    } else {
+      let converted = ((data[i].value - options[0].start) / (options[0].end - options[0].start) * 100).toFixed(2) + "%";
+      scaledValues.push(converted);
+    }
+  }
+  console.log("data heights converted to: " + scaledValues);
+
   };
 });
